9d837342df24f5fe4914b309f32dcc085eb8afbe
KhondokarAlMaruf <khondokaralmaruf01@gmail.com>
2023-07-15 00:30:05 +0600

    crud operation ready

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -25,6 +25,10 @@ const client = new MongoClient(uri, {
 
 async function run() {
   const usersCollection = client.db("super-toy-cars").collection("users");
+  const toysCollection = client.db("super-toy-cars").collection("toys");
+  const categoryListCollection = client
+    .db("super-toy-cars")
+    .collection("categoryList");
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -40,6 +44,52 @@ async function run() {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       console.log(result);
+      res.send(result);
+    });
+
+    app.get("/category", async (req, res) => {
+      const query = {};
+      const result = await categoryListCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/toys", async (req, res) => {
+      const query = {};
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    });
+
+    //post  cars in db
+    app.post("/toys", async (req, res) => {
+      const toys = req.body;
+      const result = await toysCollection.insertOne(toys);
+      res.send(result);
+    });
+
+    // get my cars
+    app.get("/my-toys", async (req, res) => {
+      const email = req.query.email;
+      const query = {
+        seller_email: email,
+      };
+      const result = await toysCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // delete product
+    app.delete("/deleteproduct/:id", async (req, res) => {
+      const deleteId = req.params.id;
+      const query = {
+        _id: new ObjectId(deleteId),
+      };
+      const result = await toysCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
